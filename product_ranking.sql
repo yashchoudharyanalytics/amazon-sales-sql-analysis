cb7c1625359238efbe23851ada0e6898e7c0acf2
yashchoudharyanalytics <yashcwork0761@gmail.com>
2025-11-17 22:40:51 +0530

    Update product_ranking.sql

--- product_ranking.sql
+++ product_ranking.sql
@@ -1,4 +1,4 @@
--- Rank products by total sales (overall)
+
 SELECT 
 p.product_name, 
 p.total_sales,
@@ -14,19 +14,3 @@ GROUP BY product_name
 ) AS p
 ORDER BY sales_rank;
 
-
--- Rank customers by total spend
-SELECT 
-c.customer_name, 
-c.total_spent,
-RANK() OVER (
-ORDER BY c.total_spent DESC
-) AS customer_rank
-FROM (
-SELECT 
-customer_name,
-SUM(total_sales) AS total_spent
-FROM sales_data
-GROUP BY customer_name 
-) AS c
-ORDER BY customer_rank;
