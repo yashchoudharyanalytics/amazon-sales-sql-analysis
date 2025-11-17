e474c57f2d9cc9ba3102e1266c4b48e92b0dbc64
yashchoudharyanalytics <yashcwork0761@gmail.com>
2025-11-17 22:37:32 +0530

    Update cumulative_and_moving_avg.sql

--- cumulative_and_moving_avg.sql
+++ cumulative_and_moving_avg.sql
@@ -13,3 +13,25 @@ FROM sales_data
 GROUP BY DATE_FORMAT(order_date, '%Y-%m-01')
 ) AS ms
 ORDER BY ms.month_start;
+
+SELECT 
+ms.month_start,
+ms.monthly_sales,
+-- 1) Cumulative running total
+SUM(ms.monthly_sales) OVER (
+ORDER BY ms.month_start
+ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW 
+) AS cumulative_sales, 
+-- 2) 3-month moving average
+AVG(ms.monthly_sales) OVER (
+ORDER BY ms.month_start
+ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+    ) AS moving_avg_3m
+FROM (
+SELECT 
+DATE_FORMAT(order_date, '%Y-%m-01') AS month_start,
+ SUM(total_sales) AS monthly_sales
+FROM sales_data
+GROUP BY DATE_FORMAT(order_date, '%Y-%m-01')
+) AS ms
+ORDER BY ms.month_start;
